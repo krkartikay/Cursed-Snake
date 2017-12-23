5b5d47e58fc2db916dc6fcbc48f53137fcc59ec5
Kumar Kartikay <krkartikay@gmail.com>
2017-12-23 14:53:48 +0530

    refactor

--- snake.py
+++ snake.py
@@ -12,9 +12,9 @@ class SnakeGame():
         self.width = width
         self.height = height
         self.snake = [] # list of positions in which snake exists
-        self.grid = [[" " for x in range(width)]
-                          for y in range(height)]
-        self.timeout = 1000-100*(level-1)
+        self.food = (0,0)
+        self.cleargrid()
+        self.timeout = 100-10*(level-1)
         self.running = True
         # Initialise Snake
         self.direction = random.choice(directions)
@@ -25,30 +25,35 @@ class SnakeGame():
         # snake shouldn't be too close to the edge
         self.snakeHead = (random.choice(range(2*init_len,width-2*init_len)),
                           random.choice(range(2*init_len,height-2*init_len)))
-        self.setGrid(self.snakeHead, 'O')
-        cur_pt = self.snakeHead
-        for i in range(5):
-            cur_pt = movept(cur_pt, reverse_direction(self.direction))
-            self.setGrid(cur_pt, '=')
+        self.food = (random.choice(range(width)),
+                     random.choice(range(height)))
+        self.cleargrid()
+        self.drawGrid()
 
     def setGrid(self, pt, value):
         # TODO: improve this fxn, use self.mode
         x,y = pt
-        try:
-            self.grid[y][x] = value
-        except IndexError as e:
-            self.running = False
-            # TODO: print "game over"
+        max_y = len(self.grid)
+        max_x = len(self.grid[0])
+        self.grid[y%max_y][x%max_x] = value
 
-    def tick(self, evt):
-        self.snakeHead = movept(self.snakeHead, self.direction)
-        self.setGrid(self.snakeHead, 'O')
+    def cleargrid():
+        self.grid = [[" " for x in range(width)]
+                          for y in range(height)]
+
+    def drawGrid():
         cur_pt = self.snakeHead
         for i in range(5):
             cur_pt = movept(cur_pt, reverse_direction(self.direction))
             self.setGrid(cur_pt, '=')
         cur_pt = movept(cur_pt, reverse_direction(self.direction))
         self.setGrid(cur_pt, ' ')
+
+    def tick(self, evt):
+        self.snakeHead = movept(self.snakeHead, self.direction)
+        self.setGrid(self.snakeHead, 'O')
+        self.cleargrid()
+        self.drawGrid()
 
     def movefwd():
         pass
