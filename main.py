1338dba0e1880ab9dc19205cf0fb98c93b1b721b
Kumar Kartikay <krkartikay@gmail.com>
2017-12-23 08:36:27 +0530

    timout logic working now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cursebox import *
 with Cursebox() as cb:
     greeting = "Hello Curses"
+    keypress = None
     while keypress!="CTRL+C":
         width, height = cb.width, cb.height
         # Center text on the screen
@@ -8,4 +9,8 @@ with Cursebox() as cb:
                y=height / 2, text=greeting,
                fg=colors.black, bg=colors.white)
         # Wait for any keypress
+        cb.screen.timeout(200)
         keypress = str(cb.poll_event())
+        greeting = str(keypress)
+        if greeting=="":
+            greeting = "SKIP"
